0854da7ab395a2aaaceb2885a295b349e2ea1bb8
Andrew <andrew208132@gmail.com>
2022-07-15 02:17:42 +0300

    Interface changed.

--- gof-gui.py
+++ gof-gui.py
@@ -2,18 +2,19 @@ import argparse
 from life import GameOfLife
 import life_gui
 
-parser = argparse.ArgumentParser(description='"Game of life" graphic version with arguments.')
-parser.add_argument("--width", default=200)
-parser.add_argument("--height", default=100)
-parser.add_argument("--cell-size", default=50)
+parser = argparse.ArgumentParser(description="'Game of life' graphic version with arguments.")
+
+parser.add_argument("--rows", default=30)
+parser.add_argument("--cols", default=75)
+parser.add_argument("--max-generations", default=20)
 
 args = parser.parse_args()
 
-width = int(args.width)
-height = int(args.height)
-cell_size = int(args.cell_size)
+rows = int(args.rows)
+cols = int(args.cols)
+max_generations = int(args.max_generations)
 
-if __name__ == '__main__':
-    life = GameOfLife((200, 100), randomize=False, max_generations=50)
+if __name__ == "__main__":
+    life = GameOfLife((rows, cols), randomize=False, max_generations=max_generations)
     gui = life_gui.GUI(life)
     gui.run()
